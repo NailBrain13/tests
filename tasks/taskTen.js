ca2f2b4379947980efdd5a9595b7ff0b5dea9e10
Алексей Казаков <75203434+NailBrain13@users.noreply.github.com>
2022-01-28 08:13:32 +0300

    10 задача разметка.

--- tasks/taskTen.js
+++ tasks/taskTen.js
@@ -3,3 +3,14 @@ import { findVowels, findVowelsAdv } from '../js/findVowels.js';
 //10 Find Vowels
 console.log(findVowels('hello'));
 console.log(findVowelsAdv('hello world'));
+
+let outputTen = document.querySelector('.output-10');
+let inputTen = document.querySelector('.input-10');
+let checkTen = document.querySelector('.check-10');
+
+const shortValue = checkTen.addEventListener('click', () => {
+  let a = inputTen.value;
+  let b = findVowelsAdv(a);
+  let c = outputTen;
+  c.textContent = `Гласные=${b}`;
+});
